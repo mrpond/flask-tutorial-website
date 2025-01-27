1c8bbe6034994e8878ffc6e4a87436a562b7bd00
Khunpon Pond <18654771+mrpond@users.noreply.github.com>
2025-01-27 19:25:53 +0700

    code format, test

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,26 +5,28 @@ import pytest
 from flaskr import create_app
 from flaskr.db import get_db, init_db, executescript
 
-with open(os.path.join(os.path.dirname(__file__), 'data.sql'), 'rb') as f:
-    _data_sql = f.read().decode('utf8')
+with open(os.path.join(os.path.dirname(__file__), "data.sql"), "rb") as f:
+    _data_sql = f.read().decode("utf8")
 
 
 @pytest.fixture
 def app():
     db_fd, db_path = tempfile.mkstemp()
 
-    app = create_app({
-        'TESTING': True,
-        'DB_TYPE': 'mariadb',
-        'SQLITE_PATH': db_path,
-        'DATABASE': {
-            "user": "root",
-            "password": "123456",
-            "host": "localhost",
-            "port": 3306,
-            "database": "flaskr",
-        },
-    })
+    app = create_app(
+        {
+            "TESTING": True,
+            "DB_TYPE": "mariadb",
+            "SQLITE_PATH": db_path,
+            "DATABASE": {
+                "user": "root",
+                "password": "123456",
+                "host": "localhost",
+                "port": 3306,
+                "database": "flaskr",
+            },
+        }
+    )
 
     with app.app_context():
         init_db()
@@ -50,15 +52,15 @@ class AuthActions(object):
     def __init__(self, client):
         self._client = client
 
-    def login(self, username='test', password='test'):
+    def login(self, username="test", password="test"):
         return self._client.post(
-            '/auth/login',
-            data={'username': username, 'password': password}
+            "/auth/login", data={"username": username, "password": password}
         )
 
     def logout(self):
-        return self._client.get('/auth/logout')
-    
+        return self._client.get("/auth/logout")
+
+
 @pytest.fixture
 def auth(client):
     return AuthActions(client)
--- tests/test_db.py
+++ tests/test_db.py
@@ -3,20 +3,22 @@ import pytest
 import mariadb
 from flaskr.db import get_db, g
 
+
 def test_get_close_db(app):
     with app.app_context():
         db = get_db()
         assert db is get_db()
-    db_type = app.config.get('DB_TYPE', 'sqlite')  # Default to SQLite
+    db_type = app.config.get("DB_TYPE", "sqlite")  # Default to SQLite
     match db_type:
-        case 'sqlite':
+        case "sqlite":
             with pytest.raises(sqlite3.ProgrammingError) as e:
-                db.execute_scalar('SELECT 1')
-                assert 'closed' in str(e.value)
-        case 'mariadb':
+                db.execute_scalar("SELECT 1")
+                assert "closed" in str(e.value)
+        case "mariadb":
             with pytest.raises(mariadb.ProgrammingError) as e:
-                db.execute_scalar('SELECT 1')
-                assert 'not connected' in str(e.value)
+                db.execute_scalar("SELECT 1")
+                assert "not connected" in str(e.value)
+
 
 def test_init_db_command(runner, monkeypatch):
     class Recorder(object):
@@ -25,7 +27,7 @@ def test_init_db_command(runner, monkeypatch):
     def fake_init_db():
         Recorder.called = True
 
-    monkeypatch.setattr('flaskr.db.init_db', fake_init_db)
-    result = runner.invoke(args=['init-db'])
-    assert 'Initialized' in result.output
+    monkeypatch.setattr("flaskr.db.init_db", fake_init_db)
+    result = runner.invoke(args=["init-db"])
+    assert "Initialized" in result.output
     assert Recorder.called
